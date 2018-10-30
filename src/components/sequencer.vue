fc18b9e1c438faa7e9b9f6b482d2a193e4aab786
The Horscht <realhorscht+github@gmail.com>
2018-10-30 15:21:35 +0100

    Implement grid with rects

--- src/components/sequencer.vue
+++ src/components/sequencer.vue
@@ -1,21 +1,11 @@
 <template>
   <div :width="width" :height="height">
   <svg width="100%" height="100%">
-<!--     <rect v-for="(note, i) in notes" :key="'note' + i"
-          :x="i * 10" width="10"
-          :y="i * 10" height="10"/> -->
-    <defs>
-      <pattern id="grid" :width="1 / 16" :height="1 / 12"
-        patternUnits="objectBoundingBox"
-        patternContentUnits="objectBoundingBox">
-        <path d="M 1 0 L 0 0 0 1" fill="none" stroke="gray" stroke-width="0.005"
-        vector-effect="non-scaling-stroke" />
-      </pattern>
-    </defs>
-
-    <rect width="100%" height="100%" fill="url(#grid)" />
-    <line x1="100%" y1="0" x2="100%" y2="100%" />
-    <line x1="0" y1="100%" x2="100%" y2="100%" />
+    <rect v-for="(note, i) in notes" :key="'note' + i"
+          :x="Math.floor(i % 16) * (1 / 16) * 100 + '%'" :width="(1 / 16) * 100 + '%'"
+          :y="Math.floor(i / 16) * (1 / 12) * 100 + '%'" :height="(1 / 12) * 100 + '%'"
+          :fill="note === 1 ? 'red' : 'black'" stroke="red" stroke-width="0.3"
+          @click="click(i)" />
   </svg>
   </div>
 </template>
@@ -33,8 +23,13 @@ export default {
       },
     },
     data: () => ({
-      notes: Array(32).fill(0)
+      notes: Array(16*12).fill(0)
     }),
+    methods: {
+      click(noteIndex) {
+        this.$set(this.notes, noteIndex, 1 - this.notes[noteIndex]);
+      }
+    }
 }
 </script>
 <style lang="scss" scoped>
