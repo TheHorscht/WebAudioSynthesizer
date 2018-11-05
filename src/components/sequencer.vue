0e284f3c43986a526150e022503912510aee8ffb
The Horscht <realhorscht+github@gmail.com>
2018-11-05 22:07:43 +0100

    Sequencer now has 2 octaves

--- src/components/sequencer.vue
+++ src/components/sequencer.vue
@@ -7,27 +7,27 @@
           y="0" height="100%"
           :class="(i-1) % 2 == 0 ? 'bg1' : 'bg2'" />
     <!-- White/Black horizontal lines -->
-    <rect v-for="i in 12" :key="'whiteBlackRect' + i"
+    <rect v-for="i in 24" :key="'whiteBlackRect' + i"
           :x="0" width="100%"
-          :y="(i-1) * 100 / 12" height="8.33%"
+          :y="(i-1) * 100 / 24" :height="100/24"
           class="blackKeyGrid"
-          v-if="[2, 4, 6, 9, 11].includes(i)" />
+          v-if="[2, 4, 6, 9, 11].includes(i%12)" />
     <!-- Empty rects -->
-    <rect v-for="(note, i) in 12*16" :key="'emptyrect' + i"
+    <rect v-for="(note, i) in 24*16" :key="'emptyrect' + i"
           :x="Math.floor(i % 16) * (100 / 16)" :width="(100 / 16)"
-          :y="Math.floor(i / 16) * (100 / 12)" :height="(100 / 12)"
+          :y="Math.floor(i / 16) * (100 / 24)" :height="(100 / 24)"
           stroke-width="0.1"
           class="note note-empty"
           @mousedown="placeNote(Math.floor(i % 16), Math.floor(i / 16))" />
     <!-- Placed notes -->
     <rect v-for="note in notes" :key="`note${note.pitch}-${note.startTick}`"
           :x="(note.startTick / totalTicks_) * 100 + '%'" :width="(1 / 16) * 100 + '%'"
-          :y="(11 - (note.pitch - 60)) * (1 / 12) * 100 + '%'" :height="(1 / 12) * 100 + '%'"
+          :y="(11 - (note.pitch - 60)) * (1 / 24) * 100 + '%'" :height="(1 / 24) * 100 + '%'"
           stroke-width="0.1"
           class="note note-placed"
           @mousedown="removeNote(note)" />
     <!-- Outlines to make sections more visible -->
-      <path v-for="i in 4" :key="'outline' + i"
+<!--       <path v-for="i in 4" :key="'outline' + i"
             d="M25 0 L0 0 0 100" :transform="`translate(${(i-1)*25},${0})`"
             class="line-outline"
             vector-effect="non-scaling-stroke" />
@@ -36,7 +36,7 @@
             vector-effect="non-scaling-stroke" />
       <path :d="`M0 ${100 / 12 * 7} L100 ${100 / 12 * 7}`"
             class="line-outline"
-            vector-effect="non-scaling-stroke" />
+            vector-effect="non-scaling-stroke" /> -->
     <!-- Playhead -->
     <line v-if="playing"
           :x1="tickP * 100" y1="0"
