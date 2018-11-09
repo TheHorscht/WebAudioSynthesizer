cab49af7d8f2467094e5c5fa23245c9d64c4a46e
The Horscht <realhorscht+github@gmail.com>
2018-11-09 19:53:05 +0100

    Fix for Firefox

--- src/voice.js
+++ src/voice.js
@@ -37,31 +37,28 @@ export default class Voice extends Observable {
   }
   noteOn(midiNote, whenTime = this.audioCtx.currentTime) {
     whenTime = Math.max(whenTime, this.audioCtx.currentTime);
-    this.biquadFilter.frequency.value = 20000;
-    this.biquadFilter.frequency.exponentialRampToValueAtTime(1000, whenTime + 0.3);
+    this.biquadFilter.frequency
+    .setValueAtTime(20000, whenTime)
+    .exponentialRampToValueAtTime(1000, whenTime + 0.3);
 
-    this.gainNode.gain.value = 0.02;
-    this.oscillatorNode.frequency.value = midiNoteToFrequency(midiNote);
+    this.gainNode.gain
+    .setValueAtTime(0.02, whenTime);
+    this.oscillatorNode.frequency.setValueAtTime(midiNoteToFrequency(midiNote), whenTime);
     this.oscillatorNode.start(whenTime);
-    // console.log(`%cOn!%c whenTime: ${whenTime}, currentTime: ${this.audioCtx.currentTime}`, 'background: green;', null)
-    // console.log(this.gainNode.gain.value)
   }
   noteOff(whenTime = this.audioCtx.currentTime) {
+    whenTime = Math.max(whenTime, this.audioCtx.currentTime);
     // https://alemangui.github.io/blog//2015/12/26/ramp-to-value.html
     // TODO: Trigger release envelope
     // Important! Setting a scheduled parameter value
-    whenTime = Math.max(whenTime, this.audioCtx.currentTime);
+    // this.gainNode.gain.cancelScheduledValues(whenTime);
+    this.gainNode.gain.setValueAtTime(0.02, whenTime);
+    this.gainNode.gain.exponentialRampToValueAtTime(0.00001, whenTime + 3);
     this.oscillatorNode.stop(whenTime + 3);
     window.setTimeout(() => {
       this.gainNode.disconnect();
       this.biquadFilter.disconnect();
+      this.dispatchEvent('voiceDonePlaying');
     }, 3000);
-    // this.gainNode.gain.cancelScheduledValues(whenTime);
-    this.gainNode.gain.setValueAtTime(this.gainNode.gain.value, whenTime);
-    this.gainNode.gain.exponentialRampToValueAtTime(0.00001, whenTime + 3);
-    window.setTimeout(() => {
-      this.dispatchEvent('voiceDonePlaying')
-    }, 50);
-    // console.log(`%cOff!%c whenTime: ${whenTime}, currentTime: ${this.audioCtx.currentTime}`, 'background: red;', null)
   }
 }
