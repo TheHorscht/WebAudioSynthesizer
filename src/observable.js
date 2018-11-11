71f07e821f88b040f913e1c9ba75d797dd81be1a
The Horscht <realhorscht+github@gmail.com>
2018-11-11 14:10:59 +0100

    Add static Observable events and props

--- src/observable.js
+++ src/observable.js
@@ -1,12 +1,14 @@
 export default class Observable {
+  static staticLookup = {};
+
   constructor() {
     this.lookup = {};
   }
   addEventListener(identifier, callback) {
-    if(!(identifier in this.lookup)) {
+    if (!(identifier in this.lookup)) {
       this.lookup[identifier] = [callback];
     } else {
-      if(callback in this.lookup[identifier]) {
+      if (callback in this.lookup[identifier]) {
         // TODO: Emit warning that callback has already been registered
       } else {
         this.lookup[identifier].push(callback);
@@ -14,10 +16,10 @@ export default class Observable {
     }
   }
   removeEventListener(identifier, callback) {
-    if(identifier in this.lookup) {
+    if (identifier in this.lookup) {
       //  && callback in this.lookup[identifier]
       const index = this.lookup[identifier].findIndex(cb => cb === callback);
-      if(index >= 0) {
+      if (index >= 0) {
         this.lookup[identifier].splice(index, 1);
       } else {
         // TODO: Emit warning that can't remove callback that hasn't been registered
@@ -27,8 +29,58 @@ export default class Observable {
     }
   }
   dispatchEvent(identifier, eventData) {
-    if(identifier in this.lookup) {
+    if (identifier in this.lookup) {
       this.lookup[identifier].forEach(callback => {
+        callback(eventData);
+      });
+    }
+  }
+
+  static createObservableMember(propName, defaultValue) {
+    Observable.staticLookup[this.name] =
+      Observable.staticLookup[this.name] || {};
+    let val = defaultValue;
+    Object.defineProperty(
+      this.prototype.constructor.prototype.constructor,
+      propName,
+      {
+        get: () => val,
+        set: newVal => {
+          val = newVal;
+          this.dispatchEvent(`${propName}Changed`, newVal);
+        },
+      },
+    );
+  }
+  static addEventListener(identifier, callback) {
+    if (!(identifier in Observable.staticLookup[this.name])) {
+      Observable.staticLookup[this.name][identifier] = [callback];
+    } else {
+      if (callback in Observable.staticLookup[this.name][identifier]) {
+        // TODO: Emit warning that callback has already been registered
+      } else {
+        Observable.staticLookup[this.name][identifier].push(callback);
+      }
+    }
+  }
+  static removeEventListener(identifier, callback) {
+    if (identifier in Observable.staticLookup[this.name]) {
+      //  && callback in this.lookup[identifier]
+      const index = Observable.staticLookup[this.name][identifier].findIndex(
+        cb => cb === callback,
+      );
+      if (index >= 0) {
+        Observable.staticLookup[this.name][identifier].splice(index, 1);
+      } else {
+        // TODO: Emit warning that can't remove callback that hasn't been registered
+      }
+    } else {
+      // TODO: Emit warning that can't remove callback that hasn't been registered
+    }
+  }
+  static dispatchEvent(identifier, eventData) {
+    if (identifier in Observable.staticLookup[this.name]) {
+      Observable.staticLookup[this.name][identifier].forEach(callback => {
         callback(eventData);
       });
     }
