28bfc3e97c3a48ee541bc45045f2953fd1bcfdd5
The Horscht <realhorscht+github@gmail.com>
2018-12-07 22:40:06 +0100

    Fix crash bug

--- src/adsr.js
+++ src/adsr.js
@@ -18,7 +18,9 @@ export default class ADSR {
     this.constantSource.start(whenTime)
   }
   noteOff(whenTime) {
-    this.constantSource.offset.cancelAndHoldAtTime(whenTime);
+    this.constantSource.offset.cancelAndHoldAtTime(whenTime + 0.0001);
     this.constantSource.offset.setTargetAtTime(0, whenTime, this.tension * this.release);
+    // Would it be good to stop? If so, figure out how long transition takes and fill in value
+    // this.constantSource.stop(whenTime + this.release);
   }
 }
